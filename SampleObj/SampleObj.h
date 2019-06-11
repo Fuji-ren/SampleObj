e936f625d68cd5b2e3ee84ece50b1131cde17227
Fuji-ren <renfu.3731@gmail.com>
2019-06-11 21:05:47 +0900

    Initial Commit

---
+++ SampleObj/SampleObj.h
@@ -0,0 +1,19 @@
+//
+//  SampleObj.h
+//  SampleObj
+//
+//  Created by 藤井廉 on 2019/06/11.
+//  Copyright © 2019年 ren.fujii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleObj.
+FOUNDATION_EXPORT double SampleObjVersionNumber;
+
+//! Project version string for SampleObj.
+FOUNDATION_EXPORT const unsigned char SampleObjVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleObj/PublicHeader.h>
+
+
